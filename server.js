ab1385817806c2964757857e8b7a70d4a4416800
Litre <litre-wu@tutanota.com>
2024-01-30 21:49:30 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -3,9 +3,43 @@ const urlLib = require('url');
 
 http.createServer((req, res) => {
     let obj = urlLib.parse(req.url, true);
-    let url = obj.pathname;
-    let params = obj.query;
-    console.log(url, params);
-    res.write('success');
-    res.end();
+    let path = obj.pathname;
+    let query = obj.query;
+
+    console.log(path, query);
+
+    const params = typeof query.params === 'string' ? new URLSearchParams(JSON.parse(query.params)) : query.params;
+    const url = params ? `${query.url}?${params}` : query.url;
+    const body = typeof query.body === 'string' ? JSON.parse(query.body) : query.body;
+    const headers = typeof query.headers === 'string' ? JSON.parse(query.headers) : query.headers;
+
+    if (query.method === 'GET') {
+        fetch(url, {
+            method: query.method,
+            headers: headers,
+        }).then((response) => response.text()).then((text) => {
+            // console.log(text);
+            res.write(text);
+            res.end();
+        }).catch(err => {
+            console.log(err);
+            res.write(text);
+            res.end();
+        });
+    }
+    if (query.method === 'POST') {
+        fetch(url, {
+            method: query.method,
+            headers: headers,
+            body: body
+        }).then((response) => response.text()).then((text) => {
+            // console.log(text);
+            res.write(text);
+            res.end();
+        }).catch(err => {
+            console.log(err);
+            res.write(text);
+            res.end();
+        });
+    }
 }).listen(8080);
